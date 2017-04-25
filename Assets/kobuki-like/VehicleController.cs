6ea19d2e78f425de435577f2f9d67aa3ff726d3e
Wayne-C <wayne60916@gmail.com>
2017-04-25 23:54:36 +0800

    Add kobuki-like vehicle

---
+++ Assets/kobuki-like/VehicleController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VehicleController : MonoBehaviour
+{
+    [SerializeField]
+    private AxleInfo axleInfo; // the information about each individual axle
+    [SerializeField]
+    private float maxMotorTorque; // maximum torque the motor can apply to wheel
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float leftMotor = motor;
+        float rightMotor = motor;
+
+        if (axleInfo.steering)
+        {
+            float steering = Input.GetAxis("Horizontal");
+
+            if (steering > 0)
+            {
+                rightMotor = rightMotor * 0.2f + 0.8f * (1f - steering);
+            }
+            else if (steering < 0)
+            {
+                leftMotor = leftMotor * 0.2f + 0.8f * (1f - Mathf.Abs(steering));
+            }
+        }
+
+        if (axleInfo.motor)
+        {
+            axleInfo.leftWheel.motorTorque = leftMotor;
+            axleInfo.rightWheel.motorTorque = rightMotor;
+        }
+    }
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
